f57460744cb1ee5d6ddd19cdf7febc1bc1d81c62
Darren Tebo <darrentebo83@outlook.com>
2020-06-14 21:57:13 -0400

    began adding restaurant section

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,10 +6,24 @@ import './Home.css';
 const Home = () => {
     return (
         <>
-            <div className='cta-container'>
+            <section className='cta-container'>
                 <h1>Your favorite food delivered while coding</h1>
                 <Button color='primary' size='lg'>Pizza?</Button>
-            </div>
+            </section>
+            <section className='primary-container'>
+                <div className='section-title'>
+                    <h2>Food Delivery in Gotham City</h2>
+                </div>
+                <div className='restaurant'>
+                    <img alt='' src='' />
+                    <h3>McDonald's</h3>
+                    <p>$ - American - Fast Food - Burgers</p>
+                    <div className='delivery-info-row'>
+                        <p className='info'>20-30 Min</p>
+                        <p className='info'>$5.99 Delivery Fee</p>
+                    </div>
+                </div>
+            </section>
         </>
     );
 };
