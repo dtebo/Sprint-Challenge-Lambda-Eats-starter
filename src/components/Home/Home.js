249a758c671516d4a4fef0b8de6d6bb8148a4ece
Darren Tebo <darrentebo83@outlook.com>
2020-06-14 22:07:12 -0400

    updated restaurant styles

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -14,13 +14,33 @@ const Home = () => {
                 <div className='section-title'>
                     <h2>Food Delivery in Gotham City</h2>
                 </div>
-                <div className='restaurant'>
-                    <img alt='' src='' />
-                    <h3>McDonald's</h3>
-                    <p>$ - American - Fast Food - Burgers</p>
-                    <div className='delivery-info-row'>
-                        <p className='info'>20-30 Min</p>
-                        <p className='info'>$5.99 Delivery Fee</p>
+                <div className='restaurant-row'>
+                    <div className='restaurant'>
+                        <img alt='' src='' />
+                        <h3>McDonald's</h3>
+                        <p>$ - American - Fast Food - Burgers</p>
+                        <div className='delivery-info-row'>
+                            <p className='info'>20-30 Min</p>
+                            <p className='info'>$5.99 Delivery Fee</p>
+                        </div>
+                    </div>
+                    <div className='restaurant'>
+                        <img alt='' src='' />
+                        <h3>sweetgreen</h3>
+                        <p>$ - Healthy - Salads</p>
+                        <div className='delivery-info-row'>
+                            <p className='info'>30-45 Min</p>
+                            <p className='info'>$4.99 Delivery Fee</p>
+                        </div>
+                    </div>
+                    <div className='restaurant'>
+                        <img alt='' src='' />
+                        <h3>Starbucks</h3>
+                        <p>$ - Cafe - Coffee &amp; Tea - Breakfast and Brunch</p>
+                        <div className='delivery-info-row'>
+                            <p className='info'>10-20 Min</p>
+                            <p className='info'>$3.99 Delivery Fee</p>
+                        </div>
                     </div>
                 </div>
             </section>
